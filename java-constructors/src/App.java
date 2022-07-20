d14de6ed2306b85a20d4d0833b0304da6ce9620a
github username <um_2003_8@hotmail.com>
2022-07-20 19:14:49 -0400

    create a class and add a constructor

--- java-constructors/src/App.java
+++ java-constructors/src/App.java
@@ -1,5 +1,11 @@
 //Constructors are special method that runs every time you create a instance of a class.
 
+class Program {
+	public Program() {
+		System.out.println("First Construtor");
+	}
+}
+
 public class App {
 	public static void main(String[] args) {
 
