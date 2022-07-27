0c9b51488598f76a7b9d85655776d0cab3622eba
github username <um_2003_8@hotmail.com>
2022-07-27 16:42:29 -0400

    create a second constructor

--- java-constructors/src/App.java
+++ java-constructors/src/App.java
@@ -5,11 +5,19 @@ class Program {
 	
 	public Program() {
 		System.out.println("First Constructor");
+		
+		value = 12;
+	}
+	
+	public Program(int value) {
+		this.value = value;
+		System.out.println("Second Contructor");
 	}
 }
 
 public class App {
 	public static void main(String[] args) {
 		Program myProgram = new Program();
+		Program myProgram1 = new Program(2);
 	}
 }
